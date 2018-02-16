de4a57fecc203791295b92ec7bb681bb319efcbd
GuanSuns <guansuns@outlook.com>
2018-02-15 21:18:52 -0600

    done about page

--- static/javascript/about/teamInfo.js
+++ static/javascript/about/teamInfo.js
@@ -1,10 +1,10 @@
 var teamInfo = [
-    ['Gijs Landwehr', '../static/img/about/jessica.jpg', 'Class of 2019', 'Drinks on average 2 gallons of milk a week.', 'Wrote the content on this page, tries and sometimes succeeds in being entertaining.', 'Github Commit: ']
-    , ['Austen Castberg', '../static/img/about/austen.png', 'Class of 2020', 'Drinks on average 4 gallons of milk a week.', 'Worked on backend.', 'Github Commit: ']
-    , ['Colin Hall', '../static/img/about/colin.png', 'Class of 2019', 'Drinks on average 4 gallons of milk a week.', 'Also worked on backend.', 'Github']
-    , ['Javier Banda', '../static/img/about/javier.png', 'Class of 2019', 'Milk deficient.', 'The third member of the backend team. The frontend people making this page don\'t quite know what that means.', 'Github']
-    , ['Diego Alcoz', '../static/img/about/diego.png', 'Class of 2019', 'Occasional milk drinker.', 'Helped on frontend to put pages together, like this one.', 'Github']
-    , ['Lin Guan', '../static/img/about/lin.png', 'Class of 2020', 'Milk deficient. Drink tea everyday', 'Responsible for the overall look and feel of the website. If it looks good, it was Lin. If it looks bad, not Lin.', 'Github']
+    ['Gijs Landwehr', '../static/img/about/gijs.png', 'Class of 2019', 'Drinks on average 2 gallons of milk a week.', 'Wrote the content on this page, tries and sometimes succeeds in being entertaining.', '0 commits 0 issue 0 unit tests']
+    , ['Austen Castberg', '../static/img/about/austen.png', 'Class of 2020', 'Drinks on average 4 gallons of milk a week.', 'Worked on backend.', '0 commits 2 issue 0 unit tests']
+    , ['Colin Hall', '../static/img/about/colin.png', 'Class of 2019', 'Drinks on average 4 gallons of milk a week.', 'Also worked on backend.', '7 commits 0 issues 0 unit tests']
+    , ['Javier Banda', '../static/img/about/javier.png', 'Class of 2019', 'Milk deficient.', 'The third member of the backend team. The frontend people making this page don\'t quite know what that means.', '0 commits 1 issue 0 unit tests']
+    , ['Diego Alcoz', '../static/img/about/diego.png', 'Class of 2019', 'Occasional milk drinker.', 'Helped on frontend to put pages together, like this one.', '1 commit 1 issue 0 unit tests']
+    , ['Lin Guan', '../static/img/about/lin.png', 'Class of 2020', 'Milk deficient. Drink tea everyday', 'Responsible for the overall look and feel of the website. If it looks good, it was Lin. If it looks bad, not Lin.', '21 commits 6 issues 0 unit tests']
 ];
 
 function getTeamInfo(){
