42fd625185376725033b92fba6075694af46d510
Javier Banda <jbanda@DN0a0fab5f.SUNet>
2018-03-15 05:03:11 -0500

    Added Google Places for food trucks

---
+++ DatabaseAPI/GooglePlaces_Trucks.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding=UTF-8
+
+import json
+import requests
+
+city = 'austin'
+
+url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?query=food+trucks+in+' \
+	+ city + '&key=AIzaSyAhABTKAw-LK6_Vh5Vhle7gwBebbpLCHew'
+
+response = requests.get(url)
+
+data = response.text
+
+parsed = json.loads(data)
+#print type(parsed)
+
+#park_name = parsed["results"]
+#print(park_name)
+
+#print type(parsed["results"]) #HMMMM it's a list
+addresses = []
+park_names = []
+place_ids = []
+ratings = []
+
+for i in parsed["results"]:
+	addresses.append(i["formatted_address"])
+	park_names.append(i["name"])
+	place_ids.append(i["place_id"])
+	ratings.append(i["rating"])
+
+#print("Address: " + address)
+
+for j in range(0,len(park_names)):
+	print("name: " + park_names[j])
+	print("address: " + addresses[j])
+	print("ratings: " + str(ratings[j]))
+	print("place_id: " + place_ids[j])
+	print(" ")
+	print("***** AYYE LMAO I'M A LEGEND ******")
+	print(" ")
