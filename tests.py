edb841a5e933045602663b16b3e50acc8f547300
dalcoz <diego.alcoz@gmail.com>
2018-03-21 22:59:20 -0500

    Add Travis config and beginning of tests

---
+++ tests.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+from unittest import main, TestCase
+from main import app
+import json
+
+class TestModels (TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+        self.app.testing = True
+
+    ############################
+    # Basic Site Functionality #
+    ############################
+
+    def test_site_home(self):
+        r = self.app.get('/')
+        self.assertEqual(r.status_code, 200)
+
+    def test_site_about(self):
+        r = self.app.get('/about')
+        self.assertEqual(r.status_code, 200)
+
+if __name__ == '__main__':
+    main()
