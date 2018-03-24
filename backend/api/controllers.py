1584aa0bbf82459498c41b53060a6de30aa2c74a
GuanSuns <guansuns@outlook.com>
2018-03-23 22:22:29 -0500

    update page size backend

--- backend/api/controllers.py
+++ backend/api/controllers.py
@@ -4,7 +4,11 @@ from api.models.truck import Truck
 from api.models.truck_photo import Truck_Photo
 
 
-truck_api_blueprint = manager.create_api_blueprint(Truck, methods=['GET'])
-truck_photo_api_blueprint = manager.create_api_blueprint(Truck_Photo, methods=['GET'])
+truck_api_blueprint = manager.create_api_blueprint(Truck, methods=['GET']
+                                                   , results_per_page=30, max_results_per_page=100)
 
-park_api_blueprint = manager.create_api_blueprint(Park, methods=['GET'])
+truck_photo_api_blueprint = manager.create_api_blueprint(Truck_Photo, methods=['GET']
+                                                         , results_per_page=30, max_results_per_page=100)
+
+park_api_blueprint = manager.create_api_blueprint(Park, methods=['GET']
+                                                  , results_per_page=30, max_results_per_page=100)
--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,3 @@
 from api import app
 
-app.run(debug=True, host='0.0.0.0', port=5000)
+app.run(debug=True, host='0.0.0.0', port=8080)
