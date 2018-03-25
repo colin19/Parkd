1a0f9f984329f2c5d2a74a9662440d62cebe7ca7
dalcoz <diego.alcoz@gmail.com>
2018-03-24 19:05:43 -0500

    Removed tests that no longer make sense

--- backend/tests.py
+++ backend/tests.py
@@ -9,18 +9,6 @@ class TestModels (TestCase):
         self.app = app.test_client()
         self.app.testing = True
 
-    ############################
-    # Basic Site Functionality #
-    ############################
-
-    def test_site_home(self):
-        r = self.app.get('/')
-        self.assertEqual(r.status_code, 200)
-
-
-    def test_site_about(self):
-        r = self.app.get('/about')
-        self.assertEqual(r.status_code, 200)
 
 if __name__ == '__main__':
     main()
